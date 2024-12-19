7364cf23adb77cc50329f5b81e33099bde91b053
EdithAguirre <edithaguirre1110@gmail.com>
2024-12-19 14:04:15 -0600

    Fixed methods for tests to pass & fixed bug 1:search functionality

--- capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -2,6 +2,7 @@ package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -44,18 +45,14 @@ public class CategoriesController {
 
     // add the appropriate annotation for a get action
     @GetMapping(path = "/{id}")
-    public Category getById(@PathVariable int id) {
+    public ResponseEntity<Category> getById(@PathVariable int id) {
         // get the category by id
-        try {
             var category = categoryDao.getById(id);
 
             if (category == null)
                 throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
-            return category;
-        } catch (Exception ex) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
-        }
+            return ResponseEntity.ok(category);
     }
 
     // the url to return all products in category 1 would look like this
@@ -74,6 +71,7 @@ public class CategoriesController {
     @PostMapping
     // add annotation to ensure that only an ADMIN can call this function
     @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ResponseStatus(HttpStatus.CREATED) // To get a 201 created status
     public Category addCategory(@RequestBody Category category) {
         // insert the category
         try {
@@ -101,7 +99,7 @@ public class CategoriesController {
     @DeleteMapping("/{id}")
     // add annotation to ensure that only an ADMIN can call this function
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public void deleteCategory(@PathVariable int id) {
+    public ResponseEntity<Void> deleteCategory(@PathVariable int id) {
         // delete the category by id
         try {
             var category = categoryDao.getById(id);
@@ -110,6 +108,7 @@ public class CategoriesController {
                 throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
             categoryDao.delete(id);
+            return ResponseEntity.noContent().build();
         } catch (Exception ex) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
         }
